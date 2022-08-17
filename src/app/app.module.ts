00828369fdb4df0d0510a830bc4f5c222329b2fa
unknown <matsekekk@gmail.com>
2022-08-17 13:36:09 +0200

    Adding unstyled form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { LeaveFormComponent } from './leave-form/leave-form.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeaveFormComponent
   ],
   imports: [
     BrowserModule,
